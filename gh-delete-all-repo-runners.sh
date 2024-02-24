423d24e5c3f1ffeb576d98630bc7a0c95e6e0011
mbehboodian <29199390+xeptore@users.noreply.github.com>
2024-02-23 22:02:32 -0600

    add gh-delete-all-repo-runners.sh

---
+++ gh-delete-all-repo-runners.sh
@@ -0,0 +1,12 @@
+curl -1sSfL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/OWNER/REPO/actions/runners |
+  jq '.runners[].id' |
+  xargs -I{} curl -1sSfL \
+  -X DELETE \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/OWNER/REPO/actions/runners/{}
