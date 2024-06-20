ba73066f5e0478a53bc67f1849b7e9918ee329d9
mbehboodian <29199390+xeptore@users.noreply.github.com>
2024-06-20 15:15:25 -0500

    Update set-nft.sh

--- set-nft.sh
+++ set-nft.sh
@@ -9,6 +9,6 @@ sysctl -eqw net.bridge.bridge-nf-call-arptables=0
 sysctl -eqw net.bridge.bridge-nf-call-ip6tables=0
 sysctl -eqw net.bridge.bridge-nf-call-iptables=0
 
-nft -f /home/nerd/sing-box/nftables.rules
+nft -f ./nftables.rules
 
 echo nameserver 127.0.1.1 > /etc/resolv.conf
