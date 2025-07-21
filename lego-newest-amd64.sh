5586783f075aa7c72188032a6e615d381e473bdc
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-07-21 16:35:48 +0200

    Create lego-newest-amd64.sh

---
+++ lego-newest-amd64.sh
@@ -0,0 +1,2 @@
+tag=$(curl -sSfL --tlsv1.3 https://api.github.com/repos/go-acme/lego/releases | jq -r 'first(.[]) | .tag_name')
+curl -SfLO --tlsv1.3 "https://github.com/go-acme/lego/releases/download/${tag}/lego_${tag}_linux_amd64.tar.gz"
