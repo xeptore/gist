8793c2d8642d04345d1c664b647fd2b0862d3a0a
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-08-10 11:59:48 +0200

    Update run-nobail.sh

--- run-nobail.sh
+++ run-nobail.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Logs the command and arguments
-# Runs directly (no extra shell)
-# Does NOT exit on failure — caller must handle error code
+# - Logs the command and arguments
+# - Runs directly (no extra shell)
+# - Does NOT exit on failure — caller must handle error code
 # Best for: scripts where errors are expected and handled manually
 
 run() { echo "+ $*"; "$@"; }
