58ffa83d3deeeb0c81b48addac53153b667ef3c2
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 16:59:30 +0200

    Update install-caddy.sh

--- install-caddy.sh
+++ install-caddy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -evuo pipefail
+set -euo pipefail
 
 apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-curl -1sSLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --yes --output /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sSLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+curl -sSLf --tlsv1.3 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --yes --output /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sSLf --tlsv1.3 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 chmod o+r /etc/apt/sources.list.d/caddy-stable.list
 apt update
