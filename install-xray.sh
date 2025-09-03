ff50edd619101e519d91de2f2f87e09fd286ec15
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 17:04:07 +0200

    Update install-xray.sh

--- install-xray.sh
+++ install-xray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -evuo pipefail
+set -euo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/XTLS/Xray-core/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sfL --tlsv1.3 -o xray.zip https://github.com/XTLS/Xray-core/releases/download/{}/Xray-linux-64.zip && unzip -o xray.zip && rm LICENSE README.md xray.zip
+curl -sSfL --tlsv1.3 'https://api.github.com/repos/XTLS/Xray-core/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sfL --tlsv1.3 -o xray.zip 'https://github.com/XTLS/Xray-core/releases/download/{}/Xray-linux-64.zip' && unzip -qo xray.zip && rm LICENSE README.md xray.zip
