ff8dc2ade5f7922411ea819058cf12ff0585b50f
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 12:02:26 +0400

    Update xray-latest-amd64.sh

--- xray-latest-amd64.sh
+++ xray-latest-amd64.sh
@@ -2,6 +2,4 @@
 
 set -exuo pipefail
 
-curl -SfLO --tlsv1.3 https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip -o Xray-linux-64.zip
-rm LICENSE README.md Xray-linux-64.zip
+curl -sSfL --tlsv1.3 'https://api.github.com/repos/XTLS/Xray-core/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sfL --tlsv1.3 -o xray.zip https://github.com/XTLS/Xray-core/releases/download/{}/Xray-linux-64.zip && unzip -o xray.zip && rm LICENSE README.md xray.zip
