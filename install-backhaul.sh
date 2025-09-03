5d7122c3c49bd055380de51862c3ba2a77a0482c
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 16:58:32 +0200

    Update install-backhaul.sh

--- install-backhaul.sh
+++ install-backhaul.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -evuo pipefail
+set -euo pipefail
 
 curl -sSfL --tlsv1.3 'https://api.github.com/repos/Musixal/Backhaul/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 'https://github.com/Musixal/Backhaul/releases/download/{}/backhaul_linux_amd64.tar.gz' | tar -xzf - backhaul
