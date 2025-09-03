024d077e5a34bb421bb92713f10d668cf53ed455
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 20:45:41 +0200

    Update install-bat.sh

--- install-bat.sh
+++ install-bat.sh
@@ -2,4 +2,20 @@
 
 set -euo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/sharkdp/bat/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} bash -c 'curl -sSfL --tlsv1.3 https://github.com/sharkdp/bat/releases/download/{}/bat-{}-x86_64-unknown-linux-musl.tar.gz | tar -xzf - bat-{}-x86_64-unknown-linux-musl/bat --strip-components=1'
+# shellcheck disable=SC1090
+source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
+
+run <<BASH
+curl \
+  -sSfL \
+  --proto '=https' \
+  --tlsv1.3 \
+  'https://api.github.com/repos/Musixal/Backhaul/releases?per_page=1&page=1' \
+  | jq '.[0].tag_name' \
+  | xargs -I {} curl \
+    -sSfL \
+    --tlsv1.3 \
+    --proto '=https' \
+    'https://github.com/Musixal/Backhaul/releases/download/{}/backhaul_linux_amd64.tar.gz' \
+  | tar -xzf - backhaul
+BASH
