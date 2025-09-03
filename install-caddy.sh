c92f3b7d9b31e96c9ad6117051161fd138647fab
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 22:30:53 +0200

    Update install-caddy.sh

--- install-caddy.sh
+++ install-caddy.sh
@@ -6,11 +6,11 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl \
-  -sSfL \
-  --tlsv1.3 \
-  --proto '=https' \
-  'https://api.github.com/repos/sharkdp/bat/releases?per_page=1&page=1' \
-  | jq '.[0].tag_name' \
-  | xargs -I {} bash -c "curl -sSfL --tlsv1.3 --proto '=https' 'https://github.com/sharkdp/bat/releases/download/{}/bat-{}-x86_64-unknown-linux-musl.tar.gz' | tar -xzf - bat-{}-x86_64-unknown-linux-musl/bat --strip-components=1"
+apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -sSLf --tlsv1.3 --proto '=https' 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --yes --output /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -sSLf --tlsv1.3 --proto '=https' 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt install -y caddy
 BASH
