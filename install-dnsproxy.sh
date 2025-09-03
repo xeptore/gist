0383b2e7aa63b3dc1a8f56bd6d81eaf4307acb6b
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 20:45:19 +0200

    Update install-dnsproxy.sh

--- install-dnsproxy.sh
+++ install-dnsproxy.sh
@@ -2,4 +2,15 @@
 
 set -euo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/AdguardTeam/dnsproxy/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 'https://github.com/AdguardTeam/dnsproxy/releases/download/{}/dnsproxy-linux-amd64-{}.tar.gz' | tar -xzf - ./linux-amd64/dnsproxy --strip-components=2
+# shellcheck disable=SC1090
+source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
+
+run <<BASH
+apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -sSLf --tlsv1.3 --proto '=https' 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --yes --output /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -sSLf --tlsv1.3 --proto '=https' 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt install -y caddy
+BASH
