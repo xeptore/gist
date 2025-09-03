dbae100fa1513315075ec5399c496a1e54511662
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 20:44:56 +0200

    Update install-dust.sh

--- install-dust.sh
+++ install-dust.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/bootandy/dust/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} bash -c 'curl -sSfL --tlsv1.3 https://github.com/bootandy/dust/releases/download/{}/dust-{}-x86_64-unknown-linux-musl.tar.gz | tar -xzf - dust-{}-x86_64-unknown-linux-musl/dust --strip-components=1'
+# shellcheck disable=SC1090
+source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
+
+run <<BASH
+curl \
+  -sSfL \
+  --tlsv1.3 \
+  --proto '=https' \
+  'https://api.github.com/repos/bootandy/dust/releases?per_page=1&page=1' \
+  | jq '.[0].tag_name' \
+  | xargs -I {} bash -c 'curl -sSfL --tlsv1.3 --proto '=https' 'https://github.com/bootandy/dust/releases/download/{}/dust-{}-x86_64-unknown-linux-musl.tar.gz' \
+  | tar -xzf - dust-{}-x86_64-unknown-linux-musl/dust --strip-components=1'
+BASH
