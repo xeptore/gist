8b3d8ce2dd2b78df4b8d2afcb00c8917d851837a
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 17:02:30 +0200

    Update install-rathole.sh

--- install-rathole.sh
+++ install-rathole.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -evuo pipefail
+set -euo pipefail
 
 curl -sSfL --tlsv1.3 'https://api.github.com/repos/rathole-org/rathole/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 -o rathole.zip 'https://github.com/rathole-org/rathole/releases/download/{}/rathole-x86_64-unknown-linux-gnu.zip' && unzip -o rathole.zip && rm rathole.zip
