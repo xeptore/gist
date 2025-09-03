4ed4aeada004eb045e8991d44b2712c994a0f345
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 14:28:17 +0200

    Update install-sing-box.sh

--- install-sing-box.sh
+++ install-sing-box.sh
@@ -2,4 +2,4 @@
 
 set -evuo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/z4x7k/sing-box-all/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 'https://github.com/z4x7k/sing-box-all/releases/download/{}/sing-box' && chmod +x sing-box
+curl -sSfL --tlsv1.3 'https://api.github.com/repos/z4x7k/sing-box-all/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfLO --tlsv1.3 'https://github.com/z4x7k/sing-box-all/releases/download/{}/sing-box' && chmod +x sing-box
