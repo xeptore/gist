053640927d68841a6612fb77e104735e0f3b05c9
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 12:47:09 +0400

    Create install-dnsproxy.sh

---
+++ install-dnsproxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+curl -sSfL --tlsv1.3 'https://api.github.com/repos/AdguardTeam/dnsproxy/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 'https://github.com/AdguardTeam/dnsproxy/releases/download/{}/dnsproxy-linux-amd64-{}.tar.gz' | tar -xzvf - ./linux-amd64/dnsproxy --strip-components=1
