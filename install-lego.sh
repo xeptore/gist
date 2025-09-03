cebc0129c9fc1352ea0a83e922409a513ef868ec
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-03 20:44:46 +0200

    Update install-lego.sh

--- install-lego.sh
+++ install-lego.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-curl -sSfL --tlsv1.3 'https://api.github.com/repos/go-acme/lego/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 'https://github.com/go-acme/lego/releases/download/{}/lego_{}_linux_amd64.tar.gz' | tar -xzf - lego
+# shellcheck disable=SC1090
+source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
+
+run <<BASH
+curl \
+  -sSfL \
+  --tlsv1.3 \
+  'https://api.github.com/repos/go-acme/lego/releases?per_page=1&page=1' \
+  | jq '.[0].tag_name' \
+  | xargs -I {} curl \
+    -sSfL \
+    --tlsv1.3 \
+    --proto '=https' \
+    'https://github.com/go-acme/lego/releases/download/{}/lego_{}_linux_amd64.tar.gz' \
+  | tar -xzvf - lego
+BASH
