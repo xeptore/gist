677d297434b8b7b61f75b951e31f045d72ab9f44
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-05 13:23:02 +0200

    Update install-sing-box.sh

--- install-sing-box.sh
+++ install-sing-box.sh
@@ -6,16 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl \
-  -sSfL \
-  --tlsv1.3 \
-  --proto '=https' \
-  'https://api.github.com/repos/z4x7k/sing-box-all/releases?per_page=1&page=1' \
-  | jq '.[0].tag_name' \
-  | xargs -I {} curl \
-    -sfLO \
-    --tlsv1.3 \
-    --proto '=https' \
-    'https://github.com/z4x7k/sing-box-all/releases/download/{}/sing-box' \
-  && chmod +x sing-box
+curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/z4x7k/sing-box-all/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sfLO --tlsv1.3 --proto '=https' 'https://github.com/z4x7k/sing-box-all/releases/download/{}/sing-box' && chmod +x sing-box
 BASH
