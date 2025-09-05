a7523ad219668f76e39ee84b01dbfa8fd26f6e94
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-05 16:04:05 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,56 +10,71 @@ fi
 # shellcheck disable=SC1090
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
-run 'cd /root'
+run_bail() {
+  local snippet
+  if (($#)); then
+    snippet="$*"
+  else
+    snippet="$(cat)"  # read from stdin (heredoc)
+  fi
 
-run 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
-run 'apt install -y jq net-tools unzip curl tree wireguard wireguard-tools nload btop'
+  run "$snippet"
+  local ec="$?"
+  if [[ "$ec" -ne 0 ]]; then
+    exit "$ec"
+  fi
+}
 
-run 'curl -sfL --tlsv1.3 --proto '=https' -o .bash_profile https://gist.xeptore.dev/.bash_profile'
-run 'source .bash_profile'
+run_bail 'cd /root'
 
-run 'curl -sfL --tlsv1.3 --proto '=https' -o .bash_aliases https://gist.xeptore.dev/.bash_aliases'
-run 'source .bash_aliases'
+run_bail 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
+run_bail 'apt install -y jq net-tools unzip curl tree wireguard wireguard-tools nload btop'
 
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-bat.sh | bash'
-run 'mv ./bat /usr/local/bin/bat'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o .bash_profile https://gist.xeptore.dev/.bash_profile'
+run_bail 'source .bash_profile'
 
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-dust.sh | bash'
-run 'mv ./dust /usr/local/bin/dust'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o .bash_aliases https://gist.xeptore.dev/.bash_aliases'
+run_bail 'source .bash_aliases'
 
-run 'mkcd /root/lego'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-lego.sh | bash'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-bat.sh | bash'
+run_bail 'mv ./bat /usr/local/bin/bat'
 
-run 'mkcd /root/xray'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-xray.sh | bash'
-run 'curl -sSfL --tlsv1.3 --proto '=https' -o server.json https://gist.xeptore.dev/xray/server.json'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-dust.sh | bash'
+run_bail 'mv ./dust /usr/local/bin/dust'
 
-run 'mkcd /root/rathole'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-rathole.sh | bash'
-run 'curl -sSfL --tlsv1.3 --proto '=https' -o server.toml https://gist.xeptore.dev/rathole/server.toml'
-run 'curl -sSfL --tlsv1.3 --proto '=https' -o client.toml https://gist.xeptore.dev/rathole/client.toml'
+run_bail 'mkcd /root/lego'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-lego.sh | bash'
 
-run 'mkcd /root/backhaul'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-backhaul.sh | bash'
+run_bail 'mkcd /root/xray'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-xray.sh | bash'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' -o server.json https://gist.xeptore.dev/xray/server.json'
 
-run 'mkcd /root/dnsproxy'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-dnsproxy.sh | bash'
-run 'curl -sSfL --tlsv1.3 --proto '=https' -o config.yaml https://gist.xeptore.dev/dnsproxy/config.yaml'
+run_bail 'mkcd /root/rathole'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-rathole.sh | bash'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' -o server.toml https://gist.xeptore.dev/rathole/server.toml'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' -o client.toml https://gist.xeptore.dev/rathole/client.toml'
 
-run 'mkcd /root/sing-box'
-run 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-sing-box.sh | bash'
+run_bail 'mkcd /root/backhaul'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-backhaul.sh | bash'
 
-run 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/xray@.service https://gist.xeptore.dev/systemd/xray@.service'
-run 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/backhaul@.service https://gist.xeptore.dev/systemd/backhaul@.service'
-run 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/dnsproxy@.service https://gist.xeptore.dev/systemd/dnsproxy@.service'
-run 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/rathole@.service https://gist.xeptore.dev/systemd/rathole@.service'
-run 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/sing-box@.service https://gist.xeptore.dev/systemd/sing-box@.service'
+run_bail 'mkcd /root/dnsproxy'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-dnsproxy.sh | bash'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' -o config.yaml https://gist.xeptore.dev/dnsproxy/config.yaml'
 
-run 'systemctl daemon-reload'
+run_bail 'mkcd /root/sing-box'
+run_bail 'curl -sSfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/install-sing-box.sh | bash'
 
-run 'curl -sfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/etc/sysctl.d/99-proxy.conf -o /etc/sysctl.d/99-proxy.conf'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/xray@.service https://gist.xeptore.dev/systemd/xray@.service'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/backhaul@.service https://gist.xeptore.dev/systemd/backhaul@.service'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/dnsproxy@.service https://gist.xeptore.dev/systemd/dnsproxy@.service'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/rathole@.service https://gist.xeptore.dev/systemd/rathole@.service'
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' -o /etc/systemd/system/sing-box@.service https://gist.xeptore.dev/systemd/sing-box@.service'
 
-run <<'BASH'
+run_bail 'systemctl daemon-reload'
+
+run_bail 'curl -sfL --tlsv1.3 --proto '=https' https://gist.xeptore.dev/etc/sysctl.d/99-proxy.conf -o /etc/sysctl.d/99-proxy.conf'
+
+run_bail <<'BASH'
 cat>~/.vimrc <<'EOF'
 set nu rnu nowrap
 syntax on
@@ -70,7 +85,7 @@ set showmode
 EOF
 BASH
 
-run <<'BASH'
+run_bail <<'BASH'
 cat>~/ufw.sh <<'EOF'
 #!/bin/bash
 
@@ -92,7 +107,7 @@ EOF
 chmod +x ~/ufw.sh
 BASH
 
-run <<'BASH'
+run_bail <<'BASH'
 cat>~/ipv6-disable.sh <<'EOF'
 #!/bin/bash
 
@@ -105,7 +120,7 @@ EOF
 chmod +x ~/ipv6-disable.sh
 BASH
 
-run <<'BASH'
+run_bail <<'BASH'
 cat>~/ipv6-enable.sh <<'EOF'
 #!/bin/bash
 
@@ -118,14 +133,13 @@ EOF
 chmod +x ~/ipv6-enable.sh
 BASH
 
-run <<'BASH'
+run_bail <<'BASH'
 cat>~/.tmux.conf <<'EOF'
 set -g default-terminal "screen"
 EOF
 BASH
 
-run 'systemctl disable --now unattended-upgrades.service'
+run_bail 'systemctl disable --now unattended-upgrades.service'
 
-run 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
-run 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
-run 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
+run_bail 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
+run_bail 'apt update && apt upgrade -y && apt autoremove -y && apt clean -y'
