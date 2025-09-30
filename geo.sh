778a6cd4ff909d2d2c6289f3065886ec20693197
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-09-30 21:07:33 +0200

    Update geo.sh

--- geo.sh
+++ geo.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-tag=\$(curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases?per_page=1&page=1' | jq '.[0].tag_name')
-curl -sfLO --tlsv1.3 --proto '=https' "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/\${tag}/geoip.dat"
-curl -sfLO --tlsv1.3 --proto '=https' "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/\${tag}/geosite.dat"
+tag=\$(curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases?per_page=1&page=1' | jq -r '.[0].tag_name')
+curl -sfLO --tlsv1.3 --proto '=https' "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/\$tag/geoip.dat"
+curl -sfLO --tlsv1.3 --proto '=https' "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/\$tag/geosite.dat"
 BASH
