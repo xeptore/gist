0c5bfbfeafa1a8206d9b009ff167f9d19de05ddd
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-10-30 09:40:33 +0330

    fix curl command to remove 's' flag

--- install-backhaul.sh
+++ install-backhaul.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl -sSfL --proto '=https' --tlsv1.3 'https://api.github.com/repos/Musixal/Backhaul/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 --proto '=https' 'https://github.com/Musixal/Backhaul/releases/download/{}/backhaul_linux_amd64.tar.gz' | tar -xzf - backhaul
+curl -sSfL --proto '=https' --tlsv1.3 'https://api.github.com/repos/Musixal/Backhaul/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -SfL --tlsv1.3 --proto '=https' 'https://github.com/Musixal/Backhaul/releases/download/{}/backhaul_linux_amd64.tar.gz' | tar -xzf - backhaul
 BASH
