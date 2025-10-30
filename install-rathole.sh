fc9eceadf7e2d94f4ea54f8529f692d1fe174850
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-10-30 09:42:10 +0330

    fix curl command to show download progress

--- install-rathole.sh
+++ install-rathole.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/rathole-org/rathole/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 -o rathole.zip 'https://github.com/rathole-org/rathole/releases/download/{}/rathole-x86_64-unknown-linux-gnu.zip' && unzip -qo rathole.zip && rm rathole.zip
+curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/rathole-org/rathole/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -SfL --tlsv1.3 -o rathole.zip 'https://github.com/rathole-org/rathole/releases/download/{}/rathole-x86_64-unknown-linux-gnu.zip' && unzip -qo rathole.zip && rm rathole.zip
 BASH
