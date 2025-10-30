d5dba1bf1f3af2fd4e31372ef38096643fce89ca
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-10-30 09:42:27 +0330

    fix curl command to show download progress

--- install-xray.sh
+++ install-xray.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/XTLS/Xray-core/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sfL --tlsv1.3 --proto '=https' -o xray.zip 'https://github.com/XTLS/Xray-core/releases/download/{}/Xray-linux-64.zip' && unzip -qo xray.zip && rm LICENSE README.md xray.zip
+curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/XTLS/Xray-core/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -SfL --tlsv1.3 --proto '=https' -o xray.zip 'https://github.com/XTLS/Xray-core/releases/download/{}/Xray-linux-64.zip' && unzip -qo xray.zip && rm LICENSE README.md xray.zip
 BASH
