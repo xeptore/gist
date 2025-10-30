bb05d2e9f31c190dd57ef01721827b6e47718873
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-10-30 09:41:57 +0330

    fix curl command to show download progress

--- install-dust.sh
+++ install-dust.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/bootandy/dust/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} bash -c 'curl -sSfL --tlsv1.3 --proto '=https' 'https://github.com/bootandy/dust/releases/download/{}/dust-{}-x86_64-unknown-linux-musl.tar.gz' | tar -xzf - dust-{}-x86_64-unknown-linux-musl/dust --strip-components=1'
+curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/bootandy/dust/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} bash -c 'curl -SfL --tlsv1.3 --proto '=https' 'https://github.com/bootandy/dust/releases/download/{}/dust-{}-x86_64-unknown-linux-musl.tar.gz' | tar -xzf - dust-{}-x86_64-unknown-linux-musl/dust --strip-components=1'
 BASH
