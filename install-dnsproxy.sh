2986d49f52f26da50663b589499b5e17e14da780
mbehboodian <29199390+xeptore@users.noreply.github.com>
2025-10-30 09:41:16 +0330

    fix curl command to remove error flag

--- install-dnsproxy.sh
+++ install-dnsproxy.sh
@@ -6,5 +6,5 @@ set -euo pipefail
 source <(curl -fsSL --proto '=https' --tlsv1.3 https://gist.xeptore.dev/run.sh)
 
 run <<BASH
-curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/AdguardTeam/dnsproxy/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -sSfL --tlsv1.3 --proto '=https' 'https://github.com/AdguardTeam/dnsproxy/releases/download/{}/dnsproxy-linux-amd64-{}.tar.gz' | tar -xzvf - ./linux-amd64/dnsproxy --strip-components=2
+curl -sSfL --tlsv1.3 --proto '=https' 'https://api.github.com/repos/AdguardTeam/dnsproxy/releases?per_page=1&page=1' | jq '.[0].tag_name' | xargs -I {} curl -SfL --tlsv1.3 --proto '=https' 'https://github.com/AdguardTeam/dnsproxy/releases/download/{}/dnsproxy-linux-amd64-{}.tar.gz' | tar -xzvf - ./linux-amd64/dnsproxy --strip-components=2
 BASH
